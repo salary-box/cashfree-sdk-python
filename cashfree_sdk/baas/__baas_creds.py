bbdbe62799a180028cdd839dda2b03457d2d83e8
Swagat Konchada <swagatata@gmail.com>
2022-10-04 23:32:07 +0530

    fix baas creds initialisation

--- cashfree_sdk/baas/__baas_creds.py
+++ cashfree_sdk/baas/__baas_creds.py
@@ -8,10 +8,10 @@ class BaasCreds:
         self.client_secret = ""
 
         # initialise client id & secreet from environment variables
-        import env
-        if env.get("CF_BAAS_CLIENT_ID") and env.get("CF_BAAS_CLIENT_SECRET"):
-            self.client_id = env.get("CF_BAAS_CLIENT_ID")
-            self.client_secret = env.get("CF_BAAS_CLIENT_SECRET")
+        import os
+        if os.environ.get("CF_BAAS_CLIENT_ID") and os.environ.get("CF_BAAS_CLIENT_SECRET"):
+            self.client_id = os.environ.get("CF_BAAS_CLIENT_ID")
+            self.client_secret = os.environ.get("CF_BAAS_CLIENT_SECRET")
 
     def set_creds(self, client_id, client_secret):
         self.client_id = client_id
