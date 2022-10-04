63091422fdd889c144198787aef4e65a9d7c7591
Swagat Konchada <swagatata@gmail.com>
2022-10-04 22:59:58 +0530

    read api keys from environment variables

--- cashfree_sdk/baas/__baas_creds.py
+++ cashfree_sdk/baas/__baas_creds.py
@@ -6,8 +6,12 @@ class BaasCreds:
     def __init__(self):
         self.client_id = ""
         self.client_secret = ""
-        self.public_key = b""
-        self.public_key_file = ""
+
+        # initialise client id & secreet from environment variables
+        import env
+        if env.get("CF_BAAS_CLIENT_ID") and env.get("CF_BAAS_CLIENT_SECRET"):
+            self.client_id = env.get("CF_BAAS_CLIENT_ID")
+            self.client_secret = env.get("CF_BAAS_CLIENT_SECRET")
 
     def set_creds(self, client_id, client_secret):
         self.client_id = client_id
