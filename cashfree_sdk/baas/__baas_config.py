46cf055149b1b203a9c05dd49235bf4491824489
Swagat Konchada <swagatata@gmail.com>
2022-10-13 19:27:33 +0530

    fix urls for baas apis

--- cashfree_sdk/baas/__baas_config.py
+++ cashfree_sdk/baas/__baas_config.py
@@ -1,11 +1,12 @@
 from .__baas_creds import BaasCreds
 
+
 class BaasConfig:
-    
+
     def __init__(self):
         self.baas_creds = BaasCreds()
         self.env = "TEST"
-        self.url = "https://payout-gamma.cashfree.com"
+        self.url = "https://test.cashfree.com"
         self.token = ""
         self.expiry = 0
         self.signature = ""
@@ -13,10 +14,11 @@ class BaasConfig:
         self.mode = "IP"
 
     def init_creds(self, client_id, client_secret, env):
-        self.baas_creds.set_creds(client_secret=client_secret, client_id=client_id)
+        self.baas_creds.set_creds(
+            client_secret=client_secret, client_id=client_id)
         self.env = env or "TEST"
         if env == "PROD":
-            self.url = "https://payout-api.cashfree.com"
+            self.url = "https://api.cashfree.com"
 
     def init_signature_creds(self, client_id, client_secret, env, **kwargs):
         self.init_creds(client_id, client_secret, env)
@@ -24,4 +26,5 @@ class BaasConfig:
         if "public_key" in kwargs:
             self.baas_creds.set_public_key(public_key=kwargs["public_key"])
         else:
-            self.baas_creds.set_public_key(public_key_file=kwargs["public_key_path"])
+            self.baas_creds.set_public_key(
+                public_key_file=kwargs["public_key_path"])
